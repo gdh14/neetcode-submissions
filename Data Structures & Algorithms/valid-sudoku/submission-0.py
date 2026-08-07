13c91a1038ca66050cc3b45a8093159f332bf3f0
gdh14 <gandahua0151@gmail.com>
2026-08-06 22:27:53 -0700

    Add: valid-sudoku - submission-0

---
+++ Data Structures & Algorithms/valid-sudoku/submission-0.py
@@ -0,0 +1,48 @@
+""" 
+check each row, keep a set per row, shoule be 
+
+# check row / column
+row_set = set()
+for num in row:
+    if num in row_set:
+        return False
+
+# check each square, same idea
+
+
+"""
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        # verify each row
+        for row_id in range(9):
+            row = board[row_id]
+            temp_set = set()
+            for num in row:
+                if num in temp_set:
+                    return False
+                elif num != '.':
+                    temp_set.add(num)
+
+        # verify each column
+        for col_id in range(9):
+            col = [board[i][col_id] for i in range(9)]
+            temp_set = set()
+            for num in col:
+                if num in temp_set:
+                    return False
+                elif num != '.':
+                    temp_set.add(num)
+
+        # verify each 3x3 grid
+        for i in range(0, 9, 3): 
+            for j in range(0, 9, 3):
+                temp_set = set()
+                for m in range(3):
+                    for n in range(3):
+                        num = board[i + m][j + n]
+                        if num in temp_set:
+                            return False
+                        elif num != '.':
+                            temp_set.add(num)
+
+        return True        
